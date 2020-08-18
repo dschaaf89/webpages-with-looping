a771fea8a4349fbd32a8d01e1ebdd050737a1437
jbnilles <jbnilles24@gmail.com>
2020-08-18 09:25:59 -0700

    change foreach loop into a for loop
    Co-authored-by: Daniel Schaaf <daniel.schaaf@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
   const icecreamFlavors = ['chocolate', 'vanilla', 'rocky road', 'strawberry'];
-icecreamFlavors.forEach(function (element) {
-  $('#icecreamList').append('<li>' + element + '</li>');
-})
+// icecreamFlavors.forEach(function (element) {
+//   $('#icecreamList').append('<li>' + element + '</li>');
+// })
+  for ( let i = 0; i < icecreamFlavors.length; i++)
+  {
+    $('#icecreamList').append('<li>' + icecreamFlavors[i] + '</li>');
+  }
+
 });
